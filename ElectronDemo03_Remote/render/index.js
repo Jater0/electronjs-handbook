284775a5cc71991cb40aa1c3d166ea36134ec3e8
Jater <1452606865@qq.com>
2021-01-10 02:45:01 +0800

    2021/1/10 second commit

--- ElectronDemo03_Remote/render/index.js
+++ ElectronDemo03_Remote/render/index.js
@@ -1,13 +1,27 @@
 const btn = this.document.querySelector("#btn")
+const btn02 = this.document.querySelector("#btn02")
 const BrowserWindow = require('electron').remote.BrowserWindow // electron remote module
 
 window.onload = () => {
     btn.onclick = () => {
         newWin = new BrowserWindow({
             width: 500,
-            heoght: 500
+            height: 500
         })
         newWin.loadFile('yellow.html')
+        newWin.on('close', () => {
+            newWin = null
+        })
+    }
+    btn02.onclick = () => {
+        var newWin = new BrowserWindow({
+            width: 500,
+            height: 500,webPreferences: {
+                nodeIntegration: true, 
+                enableRemoteModule: true
+            }
+        })
+        newWin.loadFile('index.html')
         newWin.on('close', () => {
             newWin = null
         })
